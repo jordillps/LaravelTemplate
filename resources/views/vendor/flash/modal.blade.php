58d3ff7a61278c8b15ed90f073aa1b683300331d
Jordi Llobet <jordillps@gmail.com>
2023-01-12 15:17:59 +0100

    Maquetació dels modals de confirmació

--- resources/views/vendor/flash/modal.blade.php
+++ resources/views/vendor/flash/modal.blade.php
@@ -1,4 +1,5 @@
-<div id="flash-overlay-modal" class="modal fade {{ isset($modalClass) ? $modalClass : '' }}">
+<div id="flash-overlay-modal" class="modal fade {{ isset($modalClass) ? $modalClass : '' }} 
+        {{ str_contains($body, 'successfully') ? 'success' : '' }}">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -10,7 +11,7 @@
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn  {{ str_contains($body, 'successfully') ? 'btn-success' : 'btn-default' }}" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
