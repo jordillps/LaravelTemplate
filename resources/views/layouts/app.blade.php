0f78d53c0f495508cf3ac42330512e223e23d3c3
Jordi Llobet <jordillps@gmail.com>
2023-03-29 15:11:47 +0200

    Delete current template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,20 +17,7 @@
     <link href="{{ asset('img/logoformalweb.ico') }}" rel="icon" />
     <link href="{{ asset('img/web/apple-touch-icon.png') }}" rel="apple-touch-icon" />
 
-    <!-- Bootstrap CSS File -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <!-- Libraries CSS Files -->
-    {{-- <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet" /> --}}
-    <link href="{{ asset('css/admin/fontawesome-free/all.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/web/animate/animate.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/web/ionicons/css/ionicons.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/web/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/web/lightbox/css/lightbox.min.css') }}" rel="stylesheet" />
-
-    <!-- Main Stylesheet File -->
-    <link href="{{ asset('css/web/style-red.css') }}" rel="stylesheet" />
-
+    
     
 </head>
 <body id="page-top">
@@ -51,22 +38,6 @@
     {{-- <script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/44c211e6244e57f914141ca984d442a5.js"></script> --}}
 
 
-    <!-- JavaScript Libraries -->
-    <script src="{{ asset('pluguins/js/jquery.min.js') }}"></script>
-    {{-- <script src="{{ asset('js/web/jquery-migrate.min.js') }}"></script> --}}
-    <script src="{{ asset('js/web/popper.min.js') }}"></script>
-    <script src="{{ asset('js/web/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/web/easing.min.js') }}"></script>
-    <script src="{{ asset('js/web/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('js/web/jquery.counterup.min.js') }}"></script>
-    <script src="{{ asset('js/web/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('js/web/lightbox.min.js') }}"></script>
-    <script src="{{ asset('js/web/typed.min.js') }}"></script>
-
-    <!-- Template Main Javascript File -->
-    <script src="{{ asset('js/web/main.js') }}"></script>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    
 </body>
 </html>
